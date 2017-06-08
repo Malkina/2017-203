79fd68093c18543c3adf735f054c44e4a56cfeb5
Nastya <malkina_anastasiya@mail.ru>
2017-06-08 19:12:32 +0300

    Lab#3

--- malkinaaa.cpp
+++ malkinaaa.cpp
@@ -75,7 +75,58 @@ void malkinaaa::lab2()
  */
 void malkinaaa::lab3()
 {
+long double** L = new long double*[N];
+    for (int i=0; i<N; i++)
+        L[i] = new long double[N];
+	long double* y = new long double[N];
+	long double s=0;
+	for (int i=0; i<N; i++)
+    for (int j=0; j<N; j++)
+        {
+                L[i][j]=0;
+        }
+	for (int i = 0; i < N; i++)
+        {
+            for(int k=0; k<i; k++)
+                s+=L[i][k]*L[i][k];
 
+            L[i][i]=sqrt(A[i][i]-s);
+            s = 0;
+
+        for (int j = i+1; j < N; j++)
+            {
+                for (int k=0; k<j-1; k++)
+                    s+=L[j][k]*L[i][k];
+                L[j][i] = (A[j][i]-s)/L[i][i];
+                s = 0;
+            };
+        }
+
+	for (int i = 0; i < N; i++)
+    {
+        x[i]=0;
+        y[i]=0;
+    }
+
+    y[0]=b[0]/L[0][0];
+    for (int i = 1; i < N; i++)
+    {
+        for(int j=0; j < i; j++)
+			s += L[i][j]*y[j];
+
+        y[i] = (b[i] - s)/L[i][i];
+        s = 0;
+    }
+
+	x[N-1] = y[N-1]/L[N-1][N-1];
+    for (int i=N-2;i>=0;i--)
+    {
+        for (int j=i+1;j<N;j++)
+			s += L[j][i]*x[j];
+
+        x[i] = (y[i] - s)/L[i][i];
+        s = 0;
+	}
 }
 
 
@@ -113,7 +164,28 @@ void malkinaaa::lab4()
  */
 void malkinaaa::lab5()
 {
-
+	long double eps = 0.00000001;
+    long double* p = new long double[N];
+	long double norm;
+    for (int i = 0; i < N; i++)
+        x[i]=0;
+    do {
+		for (int i = 0; i < N; i++)
+        {
+			p[i] = b[i];
+			for (int j = 0; j < N; j++)
+				if (i != j) p[i] -= A[i][j] * x[j];
+			p[i] /= A[i][i];
+		}
+        norm = fabs(x[0] - p[0]);
+		for (int h = 0; h < N; h++)
+        {
+			if (fabs(x[h] - p[h]) > norm)
+				norm = fabs(x[h] - p[h]);
+			x[h] = p[h];
+		}
+	} while (norm > eps);
+	delete[] p;
 }
 
 
@@ -123,7 +195,33 @@ void malkinaaa::lab5()
  */
 void malkinaaa::lab6()
 {
+	    long double eps = 0.00000001;
+    long double *p = new long double [N];
+    bool conv;
+    for (int i = 0; i < N; i++)
+        x[i]=0;
+    do
+    {
+        for (int i = 0; i < N; i++)
+            p[i] = x[i];
+        for (int i = 0; i < N; i++)
+        {
+            double var = 0;
+            for (int j = 0; j < i; j++)
+                var += (A[i][j] * x[j]);
+            for (int j = i + 1; j < N; j++)
+                var += (A[i][j] * p[j]);
+            x[i] = (b[i] - var) / A[i][i];
+        }
+        conv=true;
+        for (int i = 0; i < N; i++)
+            if (fabs(x[i]-p[i])>=eps){
+                conv=false;
+                break;
+            }
 
+    } while (conv==false);
+    delete[] p;
 }
 
 
